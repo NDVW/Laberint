0b5be7c80e9815b901b7ff09725118b98ea0bff6
PritomritBora <pritomrit1993@gmail.com>
2018-12-26 07:14:35 +0100

    Added Game logic For the Riddle And Answers

--- Assets/Scripts/ExampleStreaming.cs
+++ Assets/Scripts/ExampleStreaming.cs
@@ -24,7 +24,7 @@ using IBM.Watson.DeveloperCloud.DataTypes;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.AI;
-
+using TMPro;
 public class ExampleStreaming : MonoBehaviour
 {
     #region PLEASE SET THESE VARIABLES IN THE INSPECTOR
@@ -47,39 +47,62 @@ public class ExampleStreaming : MonoBehaviour
     private string _iamApikey;
     [Tooltip("The IAM url used to authenticate the apikey (optional). This defaults to \"https://iam.bluemix.net/identity/token\".")]
     [SerializeField]
+    public string _IamAccessToken;
+    [SerializeField]
     private string _iamUrl;
 
     [Header("Parameters")]
     // https://www.ibm.com/watson/developercloud/speech-to-text/api/v1/curl.html?curl#get-model
-   [Tooltip("The Model to use. This defaults to en-US_BroadbandModel")]
-   [SerializeField]
+    [Tooltip("The Model to use. This defaults to en-US_BroadbandModel")]
+    [SerializeField]
     private string _recognizeModel;
     #endregion
 
- //   public Text ResultsField;
+    //   public Text ResultsField;
     private int _recordingRoutine = 0;
     private string _microphoneID = null;
     private AudioClip _recording = null;
     private int _recordingBufferSize = 1;
     private int _recordingHZ = 22050;
     AudioSource[] audioData;
-   // GameObject[] points;
-    ShowPath path; 
+    AudioSource theme;
+    AudioSource TempAudioSpurce;
+    // GameObject[] points;
+    ShowPath path;
     private SpeechToText _service;
     Animator anim;
     GameObject enemy;
     private NavMeshAgent agent;
     GameObject wall;
-    private string _IamAccessToken = "eyJraWQiOiIyMDE3MTAzMC0wMDowMDowMCIsImFsZyI6IlJTMjU2In0.eyJpYW1faWQiOiJpYW0tU2VydmljZUlkLTA0N2MyZTM1LTAyOTEtNDU5MC1iMTBmLWY5MjE1OWU1YjgyZCIsImlkIjoiaWFtLVNlcnZpY2VJZC0wNDdjMmUzNS0wMjkxLTQ1OTAtYjEwZi1mOTIxNTllNWI4MmQiLCJyZWFsbWlkIjoiaWFtIiwiaWRlbnRpZmllciI6IlNlcnZpY2VJZC0wNDdjMmUzNS0wMjkxLTQ1OTAtYjEwZi1mOTIxNTllNWI4MmQiLCJzdWIiOiJTZXJ2aWNlSWQtMDQ3YzJlMzUtMDI5MS00NTkwLWIxMGYtZjkyMTU5ZTViODJkIiwic3ViX3R5cGUiOiJTZXJ2aWNlSWQiLCJ1bmlxdWVfaW5zdGFuY2VfY3JucyI6WyJjcm46djE6Ymx1ZW1peDpwdWJsaWM6c3BlZWNoLXRvLXRleHQ6YXUtc3lkOmEvNDk3OWI2MzkwMjljNDg3ZGJkZjhlYmQ3NjE5NGZjMjU6YTZiNGFiNTMtYjI4Yi00MTdmLWJkYmItMmI3YTEyNWIwMTE1OjoiXSwiYWNjb3VudCI6eyJ2YWxpZCI6dHJ1ZSwiYnNzIjoiNDk3OWI2MzkwMjljNDg3ZGJkZjhlYmQ3NjE5NGZjMjUifSwiaWF0IjoxNTQ0ODc0MzcyLCJleHAiOjE1NDQ4Nzc5NzIsImlzcyI6Imh0dHBzOi8vaWFtLmJsdWVtaXgubmV0L2lkZW50aXR5IiwiZ3JhbnRfdHlwZSI6InVybjppYm06cGFyYW1zOm9hdXRoOmdyYW50LXR5cGU6YXBpa2V5Iiwic2NvcGUiOiJpYm0gb3BlbmlkIiwiY2xpZW50X2lkIjoiZGVmYXVsdCIsImFjciI6MSwiYW1yIjpbInB3ZCJdfQ.gLLvvgBpe84WmaChfWtcE1bYt8ibckpuuaQiyh_LJFViDJT5DzCkUPE5sCXjgoh1-NN_byr9PxE48u7A4eyqSPHkbMWiv1jJMaCYW2cp-g3aJ5SXYa3zJCJwNmvKttk3d7717X6k-Vs_avhIgb8lYqUUQIT9F0Ly3HNH9SWpNaiBAbI0GwIf2bkEUfC80Q89J5uJme8UZ29O9iuHVQ1g9upg8wVDNykA_xYLq8InVPJ0rlKPfmggjn-6_cAoRhl9lQOQuhhC2fiWh5qZMIzVlZrFKH1cgnboGL7AE6fRqQ6Po-ZRXNen_SqX-Hd04jbK2jcxqr_lFNZYj_z1TWRK0A";
+    GameObject[] riddles;
+    GameObject ClosestRiddle;
+    int navigation_help_counter = 0;
+    private TextMeshPro textmesh;
+    GameObject[] portals;
+    GameObject portal;
+    GameObject end;
+    GameObject begin;
+    FindClosestRiddle riddleFinder = new FindClosestRiddle();
+
 
     void Start()
     {
         LogSystem.InstallDefaultReactors();
         Runnable.Run(CreateService());
         audioData = GetComponents<AudioSource>();
+        riddles = GameObject.FindGameObjectsWithTag("riddle");
         path = GetComponent<ShowPath>();
         enemy = GameObject.Find("Enemy");
-      //  wall = GameObject.Find(("TwoSided(4)"));
+        end = GameObject.Find("End");
+        begin = GameObject.Find("Begin");
+        //  portal = GameObject.Find("Portal");
+        portals = GameObject.FindGameObjectsWithTag("portal");
+        foreach (GameObject gos in portals)
+        {
+            gos.SetActive(false);
+        }
+        //  portal.SetActive(false);
+        //  wall = GameObject.Find(("TwoSided(4)"));
         anim = enemy.GetComponent<Animator>();
         agent = enemy.GetComponent<NavMeshAgent>();
         //     points = GameObject.FindGameObjectsWithTag("floortrack");
@@ -104,6 +127,7 @@ public class ExampleStreaming : MonoBehaviour
             TokenOptions tokenOptions = new TokenOptions()
             {
                 IamApiKey = _iamApikey,
+                //   IamUrl = _iamUrl
                 IamAccessToken = _IamAccessToken
 
             };
@@ -134,10 +158,10 @@ public class ExampleStreaming : MonoBehaviour
             if (value && !_service.IsListening)
             {
                 _service.RecognizeModel = (string.IsNullOrEmpty(_recognizeModel) ? "en-US_BroadbandModel" : _recognizeModel);
-                _service.DetectSilence = true;
+                _service.DetectSilence = false;
                 _service.EnableWordConfidence = true;
                 _service.EnableTimestamps = true;
-                _service.SilenceThreshold = 0.01f;
+                _service.SilenceThreshold = 0.1f;
                 _service.MaxAlternatives = 0;
                 _service.EnableInterimResults = true;
                 _service.OnError = OnError;
@@ -216,7 +240,7 @@ public class ExampleStreaming : MonoBehaviour
                 _recording.GetData(samples, bFirstBlock ? 0 : midPoint);
 
                 AudioData record = new AudioData();
-				record.MaxLevel = Mathf.Max(Mathf.Abs(Mathf.Min(samples)), Mathf.Max(samples));
+                record.MaxLevel = Mathf.Max(Mathf.Abs(Mathf.Min(samples)), Mathf.Max(samples));
                 record.Clip = AudioClip.Create("Recording", midPoint, _recording.channels, _recordingHZ, false);
                 record.Clip.SetData(samples, 0);
 
@@ -249,19 +273,19 @@ public class ExampleStreaming : MonoBehaviour
                 {
                     string text = string.Format("{0} ({1}, {2:0.00})\n", alt.transcript, res.final ? "Final" : "Interim", alt.confidence);
                     Log.Debug("ExampleStreaming.OnRecognize()", text);
-                  //  audioData.Play();
+                    //  audioData.Play();
                     //  Log.Debug("ExampleStreaming.OnRecognize()", "mama");
                     ResultsField.text = text;
 
-                      if (alt.transcript.Contains("Bob") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
-                       {
+                    if (alt.transcript.Contains("Bob") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
                         audioData[0].Play();
                         //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
                         //   Runnable.Run(Examples());
 
                     }
-                      if (alt.transcript.Contains("rock and roll") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
-                    {    
+                    if (alt.transcript.Contains("rock and roll") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
                         audioData[1].Play();
                         agent.enabled = false;
                         anim.SetInteger("Dance", 1);
@@ -269,17 +293,116 @@ public class ExampleStreaming : MonoBehaviour
                         //   Runnable.Run(Examples());
 
                     }
-                    if (alt.transcript.Contains("navigation ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    if (alt.transcript.Contains("navigate ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
                     {
-                        audioData[2].Play();
-                        path.enabled = true;
+                        if (navigation_help_counter > 0)
+                        {
+                            StartCoroutine(LowerThemeVolume(audioData[2]));
+                            audioData[2].Play();
+                            path.enabled = true;
+                            navigation_help_counter = navigation_help_counter - 1;
+                        }
+                        else
+                        {
+                            StartCoroutine(LowerThemeVolume(audioData[6]));
+                            audioData[6].Play();
+                        }
+                    }
+                    if (alt.transcript.Contains("steps ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
+                        string word = "steps";
+                        ClosestRiddle = riddleFinder.FindRiddle(riddles, transform);
+                        GameObject question1 = ClosestRiddle.transform.GetChild(0).gameObject;
+                        textmesh = ClosestRiddle.transform.GetChild(1).gameObject.GetComponent<TextMeshPro>();
+                        string answer = textmesh.text;
+
+
+
+                        if (answer.Equals(word))
+                        {
+
+                            navigation_help_counter = navigation_help_counter + 1;
+                            TextMeshPro textmesh1 = ClosestRiddle.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>();
+                            //  Renderer reward = ClosestRiddle.transform.GetChild(2).gameObject.GetComponent<Renderer>();
+                            //   question1.SetActive(false);
+                            //   reward.enabled = true;
+                            textmesh1.text = "You have Unlocked one Navigation hint!!";
+                            if (ClosestRiddle.activeSelf)
+                            {
+                                StartCoroutine(DeactivateRiddle(ClosestRiddle));
+                            }
+                            //   ClosestRiddle.transform.GetChild(0).gameObject.SetActive(false);
+                            //   ClosestRiddle.transform.GetChild(2).gameObject.Destroy();
+                        }
+                        //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
+                        //   Runnable.Run(Examples());
+
+                    }
+                    if (alt.transcript.Contains("television ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
+                        ClosestRiddle = riddleFinder.FindRiddle(riddles, transform);
+                        GameObject question = ClosestRiddle.transform.GetChild(0).gameObject;
+                        textmesh = ClosestRiddle.transform.GetChild(1).gameObject.GetComponent<TextMeshPro>();
+                        string answer = textmesh.text;
+                        GameObject portal = GameObject.Find("Portal");
+
+
+                        if (answer == "Reflection")
+                        {
+
+
+                            //  TextMeshPro textmesh1 = ClosestRiddle.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>();
+                            Renderer reward = ClosestRiddle.transform.GetChild(3).gameObject.GetComponent<Renderer>();
+                            question.SetActive(false);
+                            reward.enabled = true;
+                            //  textmesh1.text = "You have Unlocked a Portal!!";
+                            foreach (GameObject gos in portals)
+                            {
+                                gos.SetActive(true);
+                                //     
+                            }
+                            if (ClosestRiddle.activeSelf)
+                            {
+                                StartCoroutine(DeactivateRiddle(ClosestRiddle));
+                            }
+                            //   ClosestRiddle.transform.GetChild(0).gameObject.SetActive(false);
+                            //   ClosestRiddle.transform.GetChild(2).gameObject.Destroy();
+                        }
+                        //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
+                        //   Runnable.Run(Examples());
+
+                    }
+                    if (alt.transcript.Contains("tips ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
+                        ClosestRiddle = riddleFinder.FindRiddle(riddles, transform);
+                        TempAudioSpurce = ClosestRiddle.GetComponent<AudioSource>();
+                        StartCoroutine(LowerThemeVolume(TempAudioSpurce));
+                        TempAudioSpurce.Play();
+                    }
+                    if (alt.transcript.Contains("distance") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
+                        float distance_goal = Vector3.Distance(end.transform.position, transform.position);
+                        float distance_Covered = Vector3.Distance(begin.transform.position, transform.position);
+                        ClosestRiddle = riddleFinder.FindRiddle(riddles, transform);
+                        textmesh = ClosestRiddle.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>();
+                        textmesh.text = "Distance to Goal : " + distance_goal.ToString() + "             Distance covered :" + distance_Covered.ToString();
+                        //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
+                        //   Runnable.Run(Examples());
+
+                    }
+                    if (alt.transcript.Contains("enemy") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
+                    {
+                        float distance = Vector3.Distance(enemy.transform.position, transform.position);
+                        ClosestRiddle = riddleFinder.FindRiddle(riddles, transform);
+                        textmesh = ClosestRiddle.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>();
+                        textmesh.text = "DIstance to Enemy : " + distance.ToString();
                         //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
                         //   Runnable.Run(Examples());
 
                     }
                     if (alt.transcript.Contains("here ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
                     {
-                        
+
                         path.enabled = false;
                         //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
                         //   Runnable.Run(Examples());
@@ -308,15 +431,16 @@ public class ExampleStreaming : MonoBehaviour
                     }
                     if (alt.transcript.Contains("xray") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
                     {
-                        
+
                         //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
                         //   Runnable.Run(Examples());
 
                     }
                     if (alt.transcript.Contains("stop ") && ResultsField.text.Contains("Final")) // needs to be final or ECHO happens
-                    
-                    { foreach (AudioSource audio in audioData)
-                        {  
+
+                    {
+                        foreach (AudioSource audio in audioData)
+                        {
                             if (audio.isPlaying)
                             {
                                 audio.Stop();
@@ -324,7 +448,7 @@ public class ExampleStreaming : MonoBehaviour
                                 anim.SetInteger("Dance", 0);
                             }
                         }
-                        
+
                         //  _testString = "<speak version=\"1.0\"><express-as type=\"GoodNews\">I love the color of the sky too!</express-as></speak>";
                         //   Runnable.Run(Examples());
 
@@ -352,7 +476,7 @@ public class ExampleStreaming : MonoBehaviour
                     foreach (var wordAlternative in res.word_alternatives)
                     {
                         Log.Debug("ExampleStreaming.OnRecognize()", "Word alternatives found. Start time: {0} | EndTime: {1}", wordAlternative.start_time, wordAlternative.end_time);
-                        foreach(var alternative in wordAlternative.alternatives)
+                        foreach (var alternative in wordAlternative.alternatives)
                             Log.Debug("ExampleStreaming.OnRecognize()", "\t word: {0} | confidence: {1}", alternative.word, alternative.confidence);
                     }
                 }
@@ -368,6 +492,23 @@ public class ExampleStreaming : MonoBehaviour
             {
                 Log.Debug("ExampleStreaming.OnRecognize()", string.Format("speaker result: {0} | confidence: {3} | from: {1} | to: {2}", labelResult.speaker, labelResult.from, labelResult.to, labelResult.confidence));
             }
+        }
+    }
+
+    IEnumerator DeactivateRiddle(GameObject riddle)
+    {
+        yield return new WaitForSeconds(4);
+        riddle.SetActive(false);
+    }
+    IEnumerator LowerThemeVolume(AudioSource audio)
+    {
+        AudioClip clip = audio.clip;
+        float time = clip.length;
+        if (audioData[7])
+        {
+            audioData[7].volume = 0.027f;
+            yield return new WaitForSeconds(time);
+            audioData[7].volume = 0.123f;
         }
     }
 }
