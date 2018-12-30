30c3c4242088c6aefe56b7bcbd44cb084b20cbb8
Jorge Alexander <jorge@maiv.co.uk>
2018-12-30 10:58:37 +0100

    updated riddle controller

--- Assets/Scripts/Riddles/RiddleController.cs
+++ Assets/Scripts/Riddles/RiddleController.cs
@@ -1,31 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class RiddleController {
+public class RiddleController : MonoBehaviour {    
+    AnimatorStateInfo info;
+    Animator anim;
+    float distance;
+    Renderer rend;
+    bool clipInfo;
+    Material defaultMaterial;
+    Material HologramMaterial;
+    public Material[] UserMaterials;
+    Material material;
+    Material restoreMaterial;
+    private Vector3 wallDir;
     public float maxRiddleDistance = 10.0f;
-    GameObject[] riddles = GameObject.FindGameObjectsWithTag("riddle");
 
-    public string SolveRiddles(Transform _player_transform, string TextQuery) {        
-        string response = "";        
-        GameObject _ClosestRiddle = FindClosestRiddle(_player_transform);        
-        return _ClosestRiddle.solve(TextQuery);        
+    void Start () {
+        GameObject[] riddles = GameObject.FindGameObjectsWithTag("riddle");
+        rend = riddles[1].GetComponent<Renderer>();
+        defaultMaterial = rend.material;
+        HologramMaterial = defaultMaterial;        
     }
-    
-    public GameObject FindClosestRiddle(Transform _player_transform)
-    {
-        GameObject closest = null;        
-        Vector3 position = _player_transform.position;
-        float shortestDistance = Mathf.Infinity;
-
-        foreach (GameObject riddle in riddles)
-        {   
-            float riddleDistance = (riddle.transform.position - position).sqrMagnitude;
-            if (riddleDistance < shortestDistance && riddleDistance < maxRiddleDistance && riddle.Solved == false)
+	
+	void Update () {        
+        GameObject riddle = FindClosestRiddle(transform, GameObject.FindGameObjectsWithTag("riddle"));
+        
+        if (riddle != null) {            
+            anim = riddle.GetComponent<Animator>();
+            rend = riddle.GetComponent<Renderer>();
+            HologramMaterial = rend.material;
+            
+            if(defaultMaterial != HologramMaterial)
             {
-                closest = riddle;
+                restoreMaterial = defaultMaterial;
+            }
+            defaultMaterial = HologramMaterial;
+            distance = Vector3.Distance(riddle.transform.position, transform.position);
+            wallDir = (riddle.transform.position - transform.position);
+            float angle = Vector3.Dot(transform.forward, wallDir);
+            
+            if (angle > 0.8)
+            {
+                if (distance < 3)
+                {
+                    anim.SetInteger("moveback", 0);
+                    rend.sharedMaterial = UserMaterials[0];
+                    //   riddle.SetActive(true);
+                    anim.SetInteger("appear", 1);
+                    info = anim.GetCurrentAnimatorStateInfo(0);
+
+                    clipInfo = info.IsName("end");
+                    
+                    if (clipInfo == true)
+                    {
+
+                        riddle.transform.GetChild(0).gameObject.SetActive(true);
+                    }
+
+
+                }
+                if (clipInfo == true && distance > 3)
+                {
+                    anim.SetInteger("moveback", 1);
+                    anim.SetInteger("appear", 0);
+                    //     rend.sharedMaterial = restoreMaterial;
+                    riddle.transform.GetChild(0).gameObject.SetActive(false);
+                }
             }
         }
-        return closest;
+    }
+
+    public GameObject FindClosestRiddle(Transform _player_transform, GameObject[] riddles)
+    {
+        float riddleDistance;
+        float closestRiddleDistance = Mathf.Infinity;
+        GameObject closestRiddle = null;
+        
+        foreach (GameObject riddle in riddles) {
+            riddleDistance = (riddle.transform.position - _player_transform.position).sqrMagnitude;
+            
+            if (riddleDistance < closestRiddleDistance) // && riddle.Solved == false
+            {
+                closestRiddleDistance = riddleDistance;
+                closestRiddle = riddle;
+            }
+        }
+        return closestRiddle;
+    }
+
+    public string SolveRiddles(Transform _player_transform, string TextQuery) {
+        string response = "";        
+        GameObject closestRiddle = FindClosestRiddle(_player_transform, GameObject.FindGameObjectsWithTag("riddle"));
+        return response; // _ClosestRiddle.solve(TextQuery);        
     }
 }
